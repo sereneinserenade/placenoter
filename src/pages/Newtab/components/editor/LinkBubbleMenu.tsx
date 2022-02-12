91951f4de6f139706d5a36daad22893a64c1075c
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2022-02-12 22:28:16 +0100

    showing the link menu when clicking link

--- src/pages/Newtab/components/editor/LinkBubbleMenu.tsx
+++ src/pages/Newtab/components/editor/LinkBubbleMenu.tsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Editor } from '@tiptap/core'
-import { Input, Text } from '@nextui-org/react';
+import { Button, Input, Row, Text } from '@nextui-org/react';
 import { BubbleMenu } from '@tiptap/react';
+import { RiExternalLinkFill } from 'react-icons/ri';
+
+import './LinkBubbleMenu.scss'
 
 type LinkBubbleMenuProps = {
   editor: Editor,
@@ -14,13 +17,13 @@ const LinkBubbleMenu = ({ editor, currentUrl, closeLinkModal }: LinkBubbleMenuPr
 
   const [isUrlValid, setIsUrlValid] = useState<boolean>(true)
 
+  const showLinkMenu = useRef<boolean>(false)
+
+  const setShowLinkMenu = (val: boolean) => showLinkMenu.current = val
 
   useEffect(() => {
-    setTimeout(() => {
-      if (currentUrl) setUrl(currentUrl)
-      else setUrl("")
-
-    });
+    if (currentUrl) setUrl(currentUrl)
+    else setUrl("")
   }, [currentUrl])
 
   const urlPatternValidation = (url: string): boolean => {
@@ -29,34 +32,44 @@ const LinkBubbleMenu = ({ editor, currentUrl, closeLinkModal }: LinkBubbleMenuPr
   };
 
   useEffect(() => {
-    debugger
     setIsUrlValid(urlPatternValidation(url))
-    setShowLinkMenu(!!url)
+
+    setTimeout(() => setShowLinkMenu(!!url));
   }, [url])
 
   const onApply = () => isUrlValid && closeLinkModal(url)
 
-  const [showLinkMenu, setShowLinkMenu] = useState<boolean>(false)
+  const shouldShowLinkMenu = useCallback(() => showLinkMenu.current, [])
+
+  function newTab(url: string) {
+    window.open(url, "_blank");
+  }
 
   return (
-    <BubbleMenu shouldShow={() => showLinkMenu} editor={editor} className="bubble-menu link-bubble-menu" tippyOptions={{ placement: 'bottom' }}>
-      <Input
-        bordered
-        // fullWidth
-        color="primary"
-        size="sm"
-        placeholder="https://google.com"
-        // contentLeft={<RiLink />}
-        value={url}
-        onInput={(e) => setUrl((e.target as HTMLInputElement).value.trim())}
-        onKeyPress={(e) => e.key === 'Enter' && onApply()}
-        autoFocus
-        animated={false}
-      />
-      {
-        !isUrlValid && <Text color="error">
-          Please enter a valid URL.
-        </Text>
+    <BubbleMenu shouldShow={shouldShowLinkMenu} editor={editor} className="bubble-menu link-bubble-menu" tippyOptions={{ placement: 'bottom' }}>
+      {currentUrl && <section className='flex link-bubble-inner-section'>
+        <section className='flex input-button-section'>
+          <Input
+            bordered
+            size="sm"
+            placeholder="https://xyz.abc"
+            value={url}
+            onInput={(e) => setUrl((e.target as HTMLInputElement).value.trim())}
+            onKeyPress={(e) => e.key === 'Enter' && onApply()}
+            animated={false}
+          />
+
+          <Button size='sm' auto ghost icon={<RiExternalLinkFill />} onClick={() => newTab(currentUrl)} />
+        </section>
+        {
+          !isUrlValid &&
+          <section>
+            <Text color="error" size={12}>
+              Please enter a valid URL.
+            </Text>
+          </section>
+        }
+      </section>
       }
     </BubbleMenu>
   )
